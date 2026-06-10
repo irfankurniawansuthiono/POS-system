0449bb1d5d2c625a3d2628accfe59652ecc9e4a7
irfankurniawansuthiono <kurniawanmarc93@gmail.com>
2026-06-10 18:19:45 +0700

    fix: reorder imports and improve eslint configuration structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+    ...nextVitals,
+    ...nextTs,
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-empty-object-type": "warn",
+        },
+    },
+    globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts", "app/generated/**"]),
 ]);
 
 export default eslintConfig;
